52eb51b3a704e5dbfb87a0741064d7c6e6d8c1f2
sidhant97 <guptasidhant1997@gmail.com>
2024-02-26 16:27:33 +0530

    added strategy.

--- strategy/src/User.java
+++ strategy/src/User.java
@@ -3,10 +3,8 @@ public class User {
     public static void main(String[] args) {
 
         PathFinder pathFinder = new PathFinder();
-
         pathFinder.getPath("WALK");
         pathFinder.getPath("CAR");
-
         pathFinder.getPath("BUS");
 
     }
